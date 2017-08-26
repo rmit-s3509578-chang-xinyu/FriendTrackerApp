48aa70e38202b0a537f12458a8c329e489bbef31
DESKTOP-PU4U335\bowen <bowenfan32@gmail.com>
2017-08-26 21:15:42 +1000

    Add Contact intent trigger

--- src/main/java/com/example/xinyu/friendtracker/FriendFragment.java
+++ src/main/java/com/example/xinyu/friendtracker/FriendFragment.java
@@ -1,6 +1,8 @@
 package com.example.xinyu.friendtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,12 +11,16 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import static android.app.Activity.RESULT_OK;
+
 public class FriendFragment extends Fragment{
 
 	private View v;
 	private ListView listView;
 	private FloatingActionButton addButton;
 	private TextView friendView;
+
+	private Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
 
 	protected static final int PICK_CONTACTS = 100;
 
@@ -24,7 +30,7 @@ public class FriendFragment extends Fragment{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 	}
-	
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		v = inflater.inflate(R.layout.fragment_friend, container, false);
@@ -33,6 +39,24 @@ public class FriendFragment extends Fragment{
 
 		return v;
 	}
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACTS) {
+            if (resultCode == RESULT_OK) {
+                ContactDataManager contactsManager = new ContactDataManager(getActivity(), data);
+                String name = "";
+                String email = "";
+                try {
+                    name = contactsManager.getContactName();
+                    email = contactsManager.getContactEmail();
+                } catch (ContactDataManager.ContactQueryException e) {
+//                    Log.e(LOG_TAG, e.getMessage());
+                }
+            }
+        }
+    }
 
 	private void init() {
 
@@ -47,9 +71,11 @@ public class FriendFragment extends Fragment{
 			@Override
 			public void onClick(View v) {
 				friendView.append("success!");
+				startActivityForResult(contactPickerIntent, PICK_CONTACTS);
+
 			}
 		});
 	}
 
-	
+
 }
