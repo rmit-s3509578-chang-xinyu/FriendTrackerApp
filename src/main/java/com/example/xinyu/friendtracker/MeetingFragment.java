bc098a7d7a600951f5c400485337401600b7c92b
rmit-s3509578-chang-xinyu <s3509578@student.rmit.edu.au>
2017-09-01 05:13:16 +1000

    create meetingtime view

--- src/main/java/com/example/xinyu/friendtracker/MeetingFragment.java
+++ src/main/java/com/example/xinyu/friendtracker/MeetingFragment.java
@@ -5,11 +5,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MeetingFragment extends Fragment {
+
+	private TextView meetingView;
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_meeting, container, false);
+		init();
 		return v;
+
 	}
+
+	private void init() {
+
+
+		TextView meetingtimeView = (TextView)findViewById(R.id.textView10);
+
+		addButton = v.findViewById(R.id.floatingActionButton);
+		addButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivityForResult();
+
+			}
+
+			meetingButton.setOncLOCKlISTENER(new View.OnClickListener()){
+			 @Override
+                     public void onClick(View v){
+                    Toast.makeText(meetingView.this,R.string.);
+                }
+			}
+        }
+		});
 }
